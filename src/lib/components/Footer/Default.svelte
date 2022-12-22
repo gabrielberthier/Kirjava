7a98ae9ebe5c09b98a13274b1e8753e787283c04
Gabriel Nogueira Berthier da Silva <gabriel.berthier@vocedm.com.br>
2022-12-22 17:15:29 -0300

    feat: add 404 page

--- src/lib/components/Footer/Default.svelte
+++ src/lib/components/Footer/Default.svelte
@@ -1,3 +1,9 @@
+<script>
+  import { page } from '$app/stores'
+
+  $: error404 = $page.status == 404
+</script>
+
 <footer class="mt-32">
   <div class="sm:px-8">
     <div class="mx-auto max-w-7xl lg:px-8">
@@ -5,7 +11,8 @@
         <div class="relative px-4 sm:px-8 lg:px-12">
           <div class="mx-auto max-w-2xl lg:max-w-5xl">
             <div class="flex flex-col items-center justify-between gap-6 sm:flex-row">
-              <div class="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+              <div class="flex gap-6 text-sm font-medium { error404 ? 'text-white' : 'text-zinc-800 dark:text-zinc-200'}" 	
+              >
                 <a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="/about">
                   About
                 </a>
---
+++ src/lib/dom/sizing.ts
@@ -0,0 +1,19 @@
+export function getWidth(document: Document) {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  )
+}
+
+export function getHeight(document: Document) {
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.documentElement.clientHeight
+  )
+}
