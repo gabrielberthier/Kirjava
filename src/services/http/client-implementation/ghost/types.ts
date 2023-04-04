cdbcceb12b8236ff6fd133e90b5ba3b0dcfa3aca
Gabriel Nogueira Berthier da Silva <gabriel.berthier@vocedm.com.br>
2023-04-04 14:11:37 -0300

    refactor: add ghostResponse type

--- src/services/http/client-implementation/ghost/types.ts
+++ src/services/http/client-implementation/ghost/types.ts
@@ -1,21 +1,28 @@
-import type { Author, Authors, Nullable, PostOrPage, PostsOrPages, Tag, Tags } from "@tryghost/content-api";
+import type {
+  Author,
+  Authors,
+  Nullable,
+  PostOrPage,
+  PostsOrPages,
+  Tag,
+  Tags
+} from '@tryghost/content-api'
 
 export type EntitiesTypes = 'posts' | 'authors' | 'tags' | 'pages'
 
 export interface Pagination {
-    page: number;
-    limit: number;
-    pages: number;
-    total: number;
-    next: Nullable<number>;
-    prev: Nullable<number>;
+  page: number
+  limit: number
+  pages: number
+  total: number
+  next: Nullable<number>
+  prev: Nullable<number>
 }
 
-export interface Meta{
-    pagination: Pagination
+export interface Meta {
+  pagination: Pagination
 }
 
-export interface GhostResponse{
-    entity: PostOrPage | Author | Tag | PostsOrPages | Authors | Tags
-    meta?: Meta
-}
+export type GhostResponse = {
+  [index in EntitiesTypes]?: PostOrPage | Author | Tag | PostsOrPages | Authors
+} & { meta?: Meta }
