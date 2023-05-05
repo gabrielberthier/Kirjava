33302e779323b77a38e550809ed110871a4b1f75
Gabriel Nogueira Berthier da Silva <gabriel.berthier@vocedm.com.br>
2023-05-05 12:08:36 -0300

    feat: keep playing with url query params :D

--- src/lib/components/Tags/PostTag.svelte
+++ src/lib/components/Tags/PostTag.svelte
@@ -1,23 +1,30 @@
 <script lang="ts">
   import type { Tag } from '$domain/models/tag'
+  import { page } from '$app/stores'
 
   export let tag: Tag
 
-  import {goto} from '$app/navigation'
+  $: isCurrentTag = $page.url.searchParams.get('tag')?.includes(tag.slug!)
+
+  import { goto } from '$app/navigation'
 
   const setQueryParam = () => {
-    goto(`?tag=${tag.slug}`);
+    if (!isCurrentTag) {
+      goto(`?tag=${tag.slug}`)
+    }else{
+      goto('?')
+    }
   }
-
 </script>
 
 <button
   class="mr-1 inline-flex items-center leading-sm uppercase
         px-3 py-1 my-1 rounded-full bg-white dark:bg-gray-900 dark:text-white cursor-pointer
         hover:text-sky-500 text-gray-700 border
-        hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+        hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-indigo-500"
+  on:click={() => setQueryParam()}
 
-        on:click={() => setQueryParam()}
+  class:selected="{isCurrentTag}"
 >
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -42,5 +49,10 @@
 <style lang="scss">
   .tag-badge {
     font-size: 0.75rem;
+  }
+
+  .selected{
+    font-weight: 700;
+    color: #6875F5;
   }
 </style>
