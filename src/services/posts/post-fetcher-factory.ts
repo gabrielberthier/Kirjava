8bf5946cc272c16f16f13d4329107b61d461f0e8
Gabriel Nogueira <gabriel.nsilva@yduqs.com.br>
2024-09-05 11:44:06 -0300

    fix: use prerendering correctly

--- src/services/posts/post-fetcher-factory.ts
+++ src/services/posts/post-fetcher-factory.ts
@@ -1,11 +1,11 @@
 import { ApiPostsLoader } from '$services/api/posts-api'
 import { FileSystemPostsLoader } from '$services/filesystem/posts-loader'
-import { env } from '$env/dynamic/public'
+import { PUBLIC_USE_LOCAL } from '$env/static/public'
 import { PostsDataProvider } from '$lib/data/posts/posts'
 
 export class PostLoaderFactory {
   static get(): PostsDataProvider {
-    const useLocal = Boolean(env.PUBLIC_USE_LOCAL)
+    const useLocal = Boolean(PUBLIC_USE_LOCAL)
     if (useLocal) {
       return new PostsDataProvider(new FileSystemPostsLoader());
     }
